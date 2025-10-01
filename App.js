20082686edd39018d406332bee137c974326c5b8
tariq7R1X <tariquedahri04@gmail.com>
2025-10-01 22:49:31 +0500

    add readme file

---
+++ App.js
@@ -0,0 +1,25 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from Namaste React"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// =============================
+// How to Create Nested Elements
+// =============================
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading" }, "I am h1 heading of child1"),
+    React.createElement("h2", { id: "heading" }, "I am h2 heading of child1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "I am h1 heading of child2"),
+    React.createElement("h2", { id: "heading" }, "I am h2 heading of child2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,2 @@
+## CDN (Content Delivery Network):
+    - A globally distributed network of servers that delivers cached web content (HTML, CSS, JS, Images) from the nearest server to the user. 
