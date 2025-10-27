e3ccb6c81f3268796d505dc2f5787d90486df1da
tariq7R1X <tariquedahri04@gmail.com>
2025-10-27 14:46:39 +0500

    ep-07 finding the path, children routes, error element, useRouteError hook, restaurantMenu component, useParams

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -43,7 +43,7 @@ const RestaurantMenu = () => {
             </div>
             <div className="rest-image">
               <img
-                alt="restaurant-img"
+                alt="restaurant-image"
                 src={CDN_URL + restaurant.info.cloudinaryImageId}
               />
             </div>
